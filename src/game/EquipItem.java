7696173cc2c87a8098ef79ad05748a8c9997a465
choibongyong <58247736+choibongyong@users.noreply.github.com>
2020-07-02 12:03:04 +0900

    first commit

---
+++ src/game/EquipItem.java
@@ -0,0 +1,10 @@
+package game;
+
+public class EquipItem extends Item{
+
+	public EquipItem(String name, int itemCode, int itemCount, int guid, int slotIdx) {
+		super(name, itemCode, itemCount, guid);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/game/World.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.ArrayList;
+
+public class World {
+	
+	private String _worldName;
+	private ArrayList<Character> _characters;
+	
+	public World(String worldName) {
+		_worldName = worldName;
+	}
+	
+	private void addCharacter() {
+		
+	}
+
+}
